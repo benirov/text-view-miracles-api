23e2ce292bc647e70234b415b8930c6742567957
beniro <beniro.vielma@zoomcem.com>
2020-08-29 14:14:24 -0500

    primer commit para app

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports =
 {
-  port: 35735 || 35735,
+  port: process.env.PORT || 35735,
   db: process.env.MONGODB_URI || 'mongodb://develop:12345678bv@ds035735.mlab.com/textmiracles',
   SECRET_TOKEN : 'mykeytoken'
 }
