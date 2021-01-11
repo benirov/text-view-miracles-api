d83922d624f1c197a9fa2b4ae2f6f78497651a61
beniro <beniro.vielma@zoomcem.com>
2021-01-10 19:39:07 -0500

    modificacion en ruta para consumir API

--- config.js
+++ config.js
@@ -2,7 +2,9 @@
 
 module.exports =
 {
-  port: process.env.PORT || 35735,
-  db: process.env.MONGODB_URI || 'mongodb://develop:12345678bv@ds035735.mlab.com/textmiracles',
+  port: process.env.PORT || 35735, 
+  db: process.env.MONGODB_URI || 'mongodb+srv://userdefault:userdefault123@textmiracles.rb0ru.mongodb.net/textmiracles?retryWrites=true&w=majority',//'mongodb://develop:12345678bv@ds035735.mlab.com/textmiracles',
   SECRET_TOKEN : 'mykeytoken'
 }
+
+//mongodb+srv://userdefault:userdefault123@textmiracles.rb0ru.mongodb.net/textmiracles?retryWrites=true&w=majority
