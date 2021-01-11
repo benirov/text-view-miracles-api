302474635d2bcea637e49dca28aa076f4edf5a24
beniro <benirovielma.0@gmail.com>
2021-01-10 20:07:11 -0500

    modificacion en ruta para consumir API

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports =
 {
-  port: process.env.PORT || 35735, 
+  port: process.env.PORT || 35735,
   db: process.env.MONGODB_URI || 'mongodb+srv://userdefault:userdefault123@textmiracles.rb0ru.mongodb.net/textmiracles?retryWrites=true&w=majority',//'mongodb://develop:12345678bv@ds035735.mlab.com/textmiracles',
   SECRET_TOKEN : 'mykeytoken'
 }
