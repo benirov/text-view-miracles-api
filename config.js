efb1af155d9860515a784571d0947318068618df
beniro.vielma <beniro.vielma@zoomcem.com>
2022-09-13 20:16:13 -0500

    solved conflict

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 
 module.exports =
 {
-  port: process.env.PORT || 35735,
-  db: process.env.MONGODB_URI || 'localhost/textmiracles',
+  port: process.env.PORT || 4000,
+  db: process.env.MONGODB_URI || 'localhost/textmiracles?retryWrites=true&w=majority',
   SECRET_TOKEN : 'mykeytoken',
   user: process.env.USER ||  '',
   password: process.env.PASSWORD || ''
