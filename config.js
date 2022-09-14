0bdb7cd20bbac3e3cf560c91e46248f5803a4609
beniro.vielma <beniro.vielma@zoomcem.com>
2022-09-13 20:06:38 -0500

    solved problems

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 module.exports =
 {
   port: process.env.PORT || 4000,
-  db: process.env.MONGODB_URI || 'mongodb+srv://userdefault:mxqSsJpOQgQWBwse@textmiracles.rb0ru.mongodb.net/textmiracles?retryWrites=true&w=majority',
+  db: process.env.MONGODB_URI || 'localhost/textmiracles?retryWrites=true&w=majority',
   SECRET_TOKEN : 'mykeytoken',
   user: process.env.USER ||  '',
   password: process.env.PASSWORD || ''
